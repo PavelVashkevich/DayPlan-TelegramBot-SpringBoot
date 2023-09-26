91730189f773748b7ecd2ae91a4bf890c73be6f7
Pavel Vashkevich <veider300@gmail.com>
2023-09-26 23:06:21 +0200

    Initial commit

---
+++ src/main/java/com/github/pavelvashkevich/dayPlanTelegramBot/DayPlanTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.pavelvashkevich.dayPlanTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayPlanTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayPlanTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pavelvashkevich/dayPlanTelegramBot/DayPlanTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pavelvashkevich.dayPlanTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayPlanTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
